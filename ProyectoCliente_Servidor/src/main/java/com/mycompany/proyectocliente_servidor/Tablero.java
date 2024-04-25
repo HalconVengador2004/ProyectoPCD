2201df9cffea15d14c9ab74039266acc387e1b0d
Pablo-2222 <167440827+Pablo-2222@users.noreply.github.com>
2024-04-25 11:04:33 +0200

    Update Tablero.java

--- ProyectoCliente_Servidor/src/main/java/com/mycompany/proyectocliente_servidor/Tablero.java
+++ ProyectoCliente_Servidor/src/main/java/com/mycompany/proyectocliente_servidor/Tablero.java
@@ -168,20 +168,23 @@ public class Tablero {
         this.turnoJugador = turnoJugador;
     }
     public void mostrarTablero() {//version provisional para comprobar las funciones
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.print(i + " ,_");
         }
         for (int i = 10; i < 69; i++) {
             System.out.print(i + ",_");
         }
+        System.out.println("");
+        String space;
         for (int n = 0; n < NUMFICHAS; n++) {
-            for (int c = 1; c < 69; c++) {
-                if (tablero.get(c).get(n) != null) {
-                    System.out.print(tablero.get(c).get(n) + ",_");
-                } else {
+            for (int c = 1; c < 68; c++) {
+                if(tablero.get(c)==null || tablero.get(c).isEmpty()){
                     System.out.print("  ,_");
+                }else{
+                    System.out.print(tablero.get(c).get(n) + ",_");
                 }
             }
+            System.out.println("");
         }
 
     }
